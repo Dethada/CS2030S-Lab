d89bbb68c3e183b56b96c4df86c8bab8994db186
David <david@dzhy.dev>
2022-10-05 11:55:03 +0800

    Updated run.sh

--- lab4-5/run.sh
+++ lab4-5/run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-
-rm *.class ; javac -Xlint:rawtypes -Xlint:unchecked Lab4.java && ./test.sh Lab4
+if [ $# -eq 0 ];
+then
+    echo "$0: Missing arguments"
+    exit 1
+elif [ $# -gt 2 ];
+then
+    echo "$0: Too many arguments: $@"
+    exit 1
+else
+    find . -type f -name '*.class' -delete
+    currentTest="$1"
+    javac -Xlint:rawtypes -Xlint:unchecked "$currentTest.java" && ./test.sh "$currentTest"
+fi

15daf236097cd6f41ccc4bae7309d1d34fb4f806
David <david@dzhy.dev>
2022-10-14 11:45:37 +0800

    Passed Test2

--- lab06/cs2030s/fp/Memo.java
+++ lab06/cs2030s/fp/Memo.java
@@ -2,4 +2,37 @@ package cs2030s.fp;
 
 public class Memo<T> extends Lazy<T> {
   private Actually<T> value;
+
+  private Memo(T v) {
+    super(() -> v);
+    this.value = Actually.ok(v);
+  }
+
+  private Memo(Constant<T> c) {
+    super(c);
+    this.value = Actually.err(new Exception());
+  }
+
+  public static <T> Memo<T> from(T v) {
+    return (Memo<T>) new Memo<>(v);
+  }
+
+  public static <T> Memo<T> from(Constant<T> c) {
+    return (Memo<T>) new Memo<>(c);
+  }
+
+  @Override
+  public T get() {
+    return this.value.except(() -> {
+      T x = super.get();
+      this.value = Actually.ok(x);
+      return x;
+    });
+  }
+
+  @Override
+  public String toString() {
+    Immutator<Actually<String>, T> getString = x -> Actually.ok(x.toString());
+    return this.value.next(getString).unless("?");
+  }
 }

07e3e101b8b9609bdab9ba04773272243d5f02cb
David <david@dzhy.dev>
2022-10-20 19:00:03 +0800

    Added Lab07

--- lab07/Lab7.h
+++ lab07/Lab7.h
@@ -12,7 +12,6 @@ mv Combiner.java cs2030s/fp/Combiner.java
 mv Constant.java cs2030s/fp/Constant.java
 mv Immutator.java cs2030s/fp/Immutator.java
 mv Immutatorable.java cs2030s/fp/Immutatorable.java
-mv Lazy.java cs2030s/fp/Lazy.java
 mv Memo.java cs2030s/fp/Memo.java
 
 javac cs2030s/fp/*.java
